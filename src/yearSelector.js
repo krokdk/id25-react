942270fee51be0af9de608ab839f55f3e6a9f981
kristoffer <kristofferok@gmail.com>
2025-08-03 22:17:55 +0200

    id66: app henter 9999 data som default og 2025 er skjult

--- src/yearSelector.js
+++ src/yearSelector.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const YearSelector = ({ value, onChange }) => {
     const options = [
-        { value: "2025", label: "Folketingsvalg 2025" },
+       // { value: "2025", label: "Folketingsvalg 2025" },
+        { value: "9999", label: "Kommunalvalg 2025" },
+        { value: "8888", label: "Regionsrådsvalg 2025" },
         { value: "2024", label: "Europaparlamentsvalg 2024" },
         { value: "2022", label: "Folketingsvalg 2022" },
         { value: "2021", label: "Borgerforslag 2021" },
